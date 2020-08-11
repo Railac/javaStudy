b65d59f730f80bf8d0bd85077caf60de74f16961
Railac <urur700@naver.com>
2020-08-11 10:50:11 +0900

    ignore add

---
+++ VariableExample2.java
@@ -0,0 +1,9 @@
+public class VariableExample2{
+	public static void main(String[] args){
+		int n1 = 15;
+		n1 = 20;
+		int n2 = 25, n3 = 35;
+		System.out.println(n1 + " + " + n2 + "= " + (n1 +n2);
+		
+	}
+}
